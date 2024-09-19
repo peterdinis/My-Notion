4b8add275f76db60a8abae969acf768ca36d3412
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 14:22:08 +0200

    feat: Adding convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "./_components/shared/ThemeProvider";
+import { ThemeProvider } from "./_components/shared/providers/ThemeProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
