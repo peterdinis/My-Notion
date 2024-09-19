c5f18af852b8a3e362c470246da8b5c6d6ce27f4
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 15:43:01 +0200

    feat: Develoiping app part 1 WIP

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -3,17 +3,15 @@ import { PlusCircle } from "lucide-react";
 import { NextPage } from "next";
 
 const DocumentsPage: NextPage = () => {
-    return (
-        <div className="min-h-[100vh] flex flex-col items-center justify-center space-y-4">
-         <h2 className="text-lg font-medium">
-        Welcom to &apos;s MyNotion
-      </h2>
+  return (
+    <div className="min-h-[100vh] flex flex-col items-center justify-center space-y-4">
+      <h2 className="text-lg font-medium">Welcom to &apos;s MyNotion</h2>
       <Button>
         <PlusCircle className="w-4 h-4 mr-2" />
         Create a Note
       </Button>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default DocumentsPage
+export default DocumentsPage;
