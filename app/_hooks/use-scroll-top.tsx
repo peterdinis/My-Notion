a1bf590b47d79d7684324b8a3dee16696d911c1b
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 15:45:42 +0200

    feat: Write new hooks

---
+++ app/_hooks/use-scroll-top.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handelScroll = () => {
+      if (window.scrollY > threshold) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handelScroll);
+
+    return () => window.removeEventListener("scroll", handelScroll);
+  }, [threshold]);
+
+  return scrolled;
+};
