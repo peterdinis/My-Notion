26c52ef27f58a98c4eb39a31bc004becbd63e6c0
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 11:36:53 +0200

    feat: Start to design landing page

---
+++ app/_components/home/HomeHeader.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const HomeHeader: FC = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default HomeHeader;
---
+++ app/_components/home/HomeWrapper.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import HomeHeader from "./HomeHeader";
+
+const HomeWrapper: FC = () => {
+    return (
+        <div className="">
+           <div>    
+                <HomeHeader />
+           </div>
+        </div>
+    )
+}
+
+export default HomeWrapper;
