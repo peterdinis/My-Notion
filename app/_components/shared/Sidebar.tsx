193398b20aac6ec0c53f256dd3d30ea4fabe73ad
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 15:08:29 +0200

    FOO

---
+++ app/_components/shared/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Sidebar: FC = () => {
+    return (
+        <>
+            SIDEBAR
+        </>
+    )
+}
+
+export default Sidebar;
--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -2,7 +2,11 @@ import { NextPage } from "next";
 import DocumentsWrapper from "../_components/documents/DocumentsWrapper";
 
 const DocumentsPage: NextPage = () => {
-    return <DocumentsWrapper />
+    return (
+        <div>
+            <DocumentsWrapper />
+        </div>
+    )
 }
 
 export default DocumentsPage;
