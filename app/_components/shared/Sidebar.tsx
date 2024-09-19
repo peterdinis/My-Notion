44354a15434b791110f4506a1a8a7554474a3b1f
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 20:53:27 +0200

    feat: Finish baisc notion clone

--- app/_components/shared/Sidebar.tsx
+++ app/_components/shared/Sidebar.tsx
@@ -5,7 +5,7 @@ import { useParams, usePathname, useRouter } from "next/navigation";
 
 import { useMutation } from "convex/react";
 import { useMediaQuery } from "usehooks-ts";
-import { toast } from "sonner";
+import { useToast } from "@/hooks/use-toast";
 import {
   ChevronsLeft,
   MenuIcon,
@@ -21,28 +21,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { TrashBox } from "./trash-box";
-import { UserItem } from "./user-item";
-import { Navbar } from "./navbar";
-import { Item } from "./item";
-import { DocumentList } from "./document-list";
-
-import { useSearch } from "@/hooks/use-search";
-import { useSettings } from "@/hooks/use-settings";
-
 import { api } from "@/convex/_generated/api";
-
 import { cn } from "@/lib/utils";
+import useSearch from "@/app/_hooks/use-search";
+import { useSettings } from "@/app/_hooks/use-settings";
+import { DocumentList } from "../documents/DocumentList";
+import { TrashBox } from "../documents/TrashBox";
+import UserItem from "../documents/UserItem";
+import {Navigation as AppNavigation} from "./Navigation";
+import { Item } from "../documents/Item";
 
-export const Navigation = () => {
+export const Sidebar = () => {
   const router = useRouter();
   const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const search = useSearch();
   const settings = useSettings();
-  const create = useMutation(api.documents.create);
-
+  const create = useMutation(api.documents.createDocument);
+  const {toast} = useToast();
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
@@ -134,11 +131,11 @@ export const Navigation = () => {
       title: "Untitled",
     }).then((documentId) => router.push(`/documents/${documentId}`));
 
-    toast.promise(promise, {
-      loading: "Creating a new note...",
-      success: "New note created!",
-      error: "Failed to create a new note",
-    });
+    toast({
+      title: "Creating a new note...",
+      duration: 2000,
+      className: "bg-green-800 text-white font-bold text-xl"
+    })
   };
 
   return (
@@ -198,7 +195,7 @@ export const Navigation = () => {
         )}
       >
         {params.documentId ? (
-          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+          <AppNavigation isCollapsed={isCollapsed} onResetWidth={resetWidth} />
         ) : (
           <nav className="w-full bg-transparent px-3 py-2">
             {isCollapsed && (
