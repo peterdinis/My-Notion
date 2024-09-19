d8bcdc8f97b531c24ea8d7200fe31bd2f27ddadf
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 19:29:25 +0200

    FOO

--- app/_components/shared/Sidebar.tsx
+++ app/_components/shared/Sidebar.tsx
@@ -1,11 +1,11 @@
-"use client"; // Ensures this file is executed in the client-side environment
-import UseSearch from "@/app/_hooks/use-search";
-import { api } from "@/convex/_generated/api";
-import { useToast } from "@/hooks/use-toast";
-import { cn } from "@/lib/utils"; // Import a utility function for conditional class names
-import { Item } from "@radix-ui/react-dropdown-menu";
-import { Popover, PopoverTrigger, PopoverContent } from "@radix-ui/react-popover";
+"use client";
+
+import React, { useRef, ElementRef, useState, useEffect } from "react";
+import { useParams, usePathname, useRouter } from "next/navigation";
+
 import { useMutation } from "convex/react";
+import { useMediaQuery } from "usehooks-ts";
+import { toast } from "sonner";
 import {
   ChevronsLeft,
   MenuIcon,
@@ -14,211 +14,198 @@ import {
   Search,
   Settings,
   Trash,
-} from "lucide-react"; // Import icons from the Lucide React library
-import { useParams, usePathname, useRouter } from "next/navigation"; // Import a hook to get the current pathname
-import React, { ElementRef, useEffect, useRef, useState } from "react"; // Import React and necessary hooks
-import { useMediaQuery } from "usehooks-ts"; // Import a hook to handle media queries
-import Navbar from "./Navbar";
-import { useSettings } from "@/app/_hooks/use-settings";
+} from "lucide-react";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
+import { UserItem } from "./user-item";
+import { Navbar } from "./navbar";
+import { Item } from "./item";
+import { DocumentList } from "./document-list";
 
-function Sidebar() {
+import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
+
+import { api } from "@/convex/_generated/api";
+
+import { cn } from "@/lib/utils";
+
+export const Navigation = () => {
   const router = useRouter();
-  const settings = useSettings();
-  const search = UseSearch();
   const params = useParams();
-  const pathName = usePathname(); // Get the current pathname
-  const isMobile = useMediaQuery("(max-width: 768px)"); // Check if the screen width is 768px or less (mobile view)
-  const createDocument = useMutation(api.documents.createDocument); // Mutation to create a new document
-  const isResizingRef = useRef(false); // Ref to track if the sidebar is being resized
-  const sideBarRef = useRef<ElementRef<"aside">>(null); // Ref for the sidebar element
-  const nevBarRef = useRef<ElementRef<"div">>(null); // Ref for the Sidebar bar element
-  const [isResetting, setIsResetting] = useState(false); // State to track if the sidebar is resetting
-  const [isCollapsed, setIsCollapsed] = useState(isMobile); // State to track if the sidebar is collapsed
-  const {toast} = useToast();
-  // Function to handle when the user starts resizing the sidebar
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const search = useSearch();
+  const settings = useSettings();
+  const create = useMutation(api.documents.create);
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
+
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    event.preventDefault(); // Prevent default behavior
-    event.stopPropagation(); // Stop the event from bubbling up
-    isResizingRef.current = true; // Set resizing to true
-    document.addEventListener("mousemove", handleMouseMove); // Add a listener to track mouse movements
-    document.addEventListener("mouseup", handleMouseUp); // Add a listener to handle when the user stops resizing
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
   };
 
-  // Function to handle the resizing of the sidebar
-  const handleMouseMove = (event: MouseEvent) => {
-    if (!isResizingRef.current) {
-      return; // If not resizing, do nothing
-    }
-    let newWidth = event.clientX; // Get the new width based on the mouse position
-    if (newWidth < 240) {
-      newWidth = 240; // Set a minimum width of 240px
-    }
-    if (newWidth > 480) {
-      newWidth = 480; // Set a maximum width of 480px
-    }
-    if (sideBarRef.current && nevBarRef.current) {
-      sideBarRef.current.style.width = `${newWidth}px`; // Adjust the sidebar width
-      nevBarRef.current.style.setProperty("left", `${newWidth}px`); // Adjust the navbar position
-      nevBarRef.current.style.setProperty(
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isResizingRef.current) return;
+
+    let newWidth = e.clientX;
+
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
         "width",
         `calc(100% - ${newWidth}px)`
-      ); // Adjust the navbar width
+      );
     }
   };
 
-  // Function to handle when the user stops resizing the sidebar
   const handleMouseUp = () => {
-    isResizingRef.current = false; // Set resizing to false
-    document.removeEventListener("mousemove", handleMouseMove); // Remove the mousemove listener
-    document.removeEventListener("mouseup", handleMouseUp); // Remove the mouseup listener
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
   };
 
-  // Function to reset the sidebar to its default width
   const resetWidth = () => {
-    if (sideBarRef.current && nevBarRef.current) {
-      setIsCollapsed(false); // Set the sidebar to not collapsed
-      setIsResetting(true); // Set the resetting state to true
-      sideBarRef.current.style.width = isMobile ? "100%" : "240px"; // Set width based on screen size
-      nevBarRef.current.style.setProperty(
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty(
         "width",
-        isMobile ? "100%" : "calc(100% - 240px)"
-      ); // Adjust the navbar width based on screen size
-      nevBarRef.current.style.setProperty("left", isMobile ? "100%" : "240px"); // Adjust the navbar position
-      setTimeout(() => {
-        setIsResetting(false); // After animation, set resetting to false
-      }, 300); // Wait for 300ms
+        isMobile ? "0" : `calc(100% - 240px)`
+      );
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+
+      setTimeout(() => setIsResetting(false), 300);
     }
   };
 
-  // Function to collapse the sidebar
   const collapse = () => {
-    if (sideBarRef.current && nevBarRef.current) {
-      setIsCollapsed(true); // Set the sidebar to collapsed
-      setIsResetting(true); // Set the resetting state to true
-      sideBarRef.current.style.width = "0px"; // Collapse the sidebar
-      nevBarRef.current.style.setProperty("width", "100%"); // Set the navbar width to 100%
-      nevBarRef.current.style.setProperty("left", "0"); // Set the navbar position to the start
-      setTimeout(() => {
-        setIsResetting(false); // After animation, set resetting to false
-      }, 300); // Wait for 300ms
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+
+      setTimeout(() => setIsResetting(false), 300);
     }
   };
 
-  // Effect to handle screen size changes
-  useEffect(() => {
-    if (isMobile) {
-      collapse(); // Collapse the sidebar if on mobile
-    } else {
-      resetWidth(); // Reset the sidebar width if not on mobile
-    }
-  }, [isMobile]);
+  const handleCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    }).then((documentId) => router.push(`/documents/${documentId}`));
 
-  // Effect to handle path changes
-  useEffect(() => {
-    if (isMobile) {
-      collapse(); // Collapse the sidebar when the path changes (if not on mobile)
-    }
-  }, [pathName, isMobile]);
-
-  // Function to create a new document
-  const handleCreateDocument = () => {
-    const promise = createDocument({ title: "Untitled" }).then((documentId) =>
-      router.push(`/documents/${documentId}`)
-    ); // Create a new document with a default title
-
-    toast({
-        title: "Create a new node" + promise,
-        duration: 2000,
-        className: "bg-green-800 text-white font-bold"
-    })
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note",
+    });
   };
 
   return (
     <>
-      {/* Sidebar */}
       <aside
-        ref={sideBarRef}
+        ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full  bg-secondary overflow-y-auto sticky felx w-60 flex-col z-[99999] min-h-[100vh] left-0 top-0",
-          isResetting && "transition-all ease-in-out duration-300", // Apply transition during reset
-          isMobile && "w-0" // Set width to 0 if on mobile
+          "group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
         )}
       >
-        {/* ChevronsLeft Icon to collapse the sidebar */}
         <div
           onClick={collapse}
           role="button"
           className={cn(
-            "absolute top-3 right-2  w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100",
-            isMobile && "opacity-100" // Always show on mobile
+            "absolute right-2 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300 group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600",
+            isMobile && "opacity-100"
           )}
         >
-          <ChevronsLeft className="w-6 h-6" />{" "}
+          <ChevronsLeft className="h-6 w-6" />
         </div>
-
-        {/* Some Actions */}
         <div>
-          <UserItem /> {/* Render the UserItem component */}
-          {/* Render the Search item */}
-          <Item lable="Search" icon={Search} isSearch onClick={search.onOpen} />
-          {/* Render the Settings item */}
-          <Item lable="Setting" icon={Settings} onClick={settings.onOpen} />
-          {/* Render the New Page item */}
-          <Item
-            onClick={handleCreateDocument}
-            lable="New Page"
-            icon={PlusCircle}
-          />
+          <UserItem />
+          <Item label="Search" icon={Search} onClick={search.onOpen} isSearch />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
+          <Item label="New page" icon={PlusCircle} onClick={handleCreate} />
+        </div>
+        <div className="mt-4">
+          <DocumentList />
+          <Item label="Add a page" icon={Plus} onClick={handleCreate} />
           <Popover>
-            <PopoverTrigger className="w-full mt-4">
-              <Item lable="Trash" icon={Trash} />
+            <PopoverTrigger className="mt-4 w-full">
+              <Item label="Trash" icon={Trash} />
             </PopoverTrigger>
             <PopoverContent
-              side={isMobile ? "bottom" : "right"}
               className="w-72 p-0"
+              side={isMobile ? "bottom" : "right"}
             >
               <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
-        {/* Render the list of documents */}
-        <div className="mt-4">
-          <DocumentList />
-          <Item onClick={handleCreateDocument} icon={Plus} lable="Add Page" />
-        </div>
-
-        {/* Handle for resizing the sidebar */}
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
-          className="opacity-0 group-hover/sidebar:opacity-100
-        transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0
-        "
+          className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100"
         />
       </aside>
 
-      {/* Navbar */}
       <div
-        ref={nevBarRef}
+        ref={navbarRef}
         className={cn(
-          "absolute top-0 left-60 z-[99999] w-[calc(100%-240px)] ",
-          isResetting && "transition-all ease-in-out duration-300", // Apply transition during reset
-          isMobile && "w-full left-0" // Adjust width and position for mobile
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full"
         )}
       >
-        {!!params.documentId ? (
+        {params.documentId ? (
           <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
         ) : (
-          /* Icon to open the sidebar when collapsed */
-          <nav className="bg-transparent px-3 py-2 w-full">
+          <nav className="w-full bg-transparent px-3 py-2">
             {isCollapsed && (
               <MenuIcon
-                onClick={resetWidth}
                 role="button"
-                className="w-6 h-6 text-muted-foreground"
+                onClick={resetWidth}
+                className="h-6 w-6 text-muted-foreground"
               />
             )}
           </nav>
@@ -226,6 +213,4 @@ function Sidebar() {
       </div>
     </>
   );
-}
-
-export default Sidebar;
+};
