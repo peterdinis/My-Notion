eae8ba2a14645ee4dae9f0a73eca0ac1f50dbf3c
Peter Dinis <peterdinis0@pobox.sk>
2024-09-24 16:56:11 +0200

    FOO

--- lib/edgestore.ts
+++ lib/edgestore.ts
@@ -1,6 +1,7 @@
 'use client';
 import { type EdgeStoreRouter } from '@/app/api/edgestore/[...egdestore]/route'
 import { createEdgeStoreProvider } from '@edgestore/react';
+
 const { EdgeStoreProvider, useEdgeStore } =
     createEdgeStoreProvider<EdgeStoreRouter>();
 export { EdgeStoreProvider, useEdgeStore };
